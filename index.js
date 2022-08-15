47df4f4fa660dd1f5fd93746573d4e2121584779
Isaac-Machini <isaac.machini@student.moringaschool.com>
2022-08-15 10:38:09 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+//function to find sum of numbers
+var a,b
+function add(a,b) {
+    return (a+b)
+}
+
+
+//function to find difference of numbers
+function subtract(a,b) {
+    return (a-b)
+
+}
+
+//function to calculate numbers
+function multiply(a,b) {
+    return(a*b)
+}
+
+
+function divide(a,b) {
+    return(a/b)
+
+}
+
+function increment(a) {
+    return(a+1);
+}
+
+
+function decrement(a) {
+    return(a-1);
+}
+function makeInt(a) {
+    parseInt(a, 10);{
+        return (a);
+    }
+}
+
+function makeInt(n){
+    let number = parseInt(n,10);
+    return number
+}
+
+function preserveDecimal(n) {
+    let float = parseFloat(n);
+  return float
+}
+
+
